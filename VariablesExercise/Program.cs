a5df218bfeb3d3d6886719c6181f48f6e3ef0e1c
KellyAllen013069 <89815777+KellyAllen013069@users.noreply.github.com>
2024-07-06 13:11:06 -0500

    exercise complete

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string name = "Kelly";
+            int exerciseNumber = 4;
+            char lastInitial = 'A';
+            bool isCoding = true;
+            double percentComplete = 3.5;
+            decimal worth = 1.95m;
+            Console.Write(name + " " + lastInitial + " is working on exercise " + exerciseNumber + ". Currently, she is " );
+            Console.Write(isCoding ? "coding." : "not coding");
+            Console.WriteLine(" She is " + percentComplete + "% complete with this project worth $" + worth);
         }
     }
 }
